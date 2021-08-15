02806a770b27c7f7235631cc0b8f1b290334e3f3
tanelc <moin@taniaelcome.de>
2021-08-15 11:47:24 +0200

    improving navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
